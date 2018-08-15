42106ef7ba5711caddba88220cdf61659d170366
caroline <huangxm5954@icloud.com>
2018-08-15 16:12:42 +0800

    update code

--- src/main/java/com/xxg/websocket/LogWebSocketHandle.java
+++ src/main/java/com/xxg/websocket/LogWebSocketHandle.java
@@ -22,7 +22,8 @@ public class LogWebSocketHandle {
 	public void onOpen(Session session) {
 		try {
 			// 执行tail -f命令
-			process = Runtime.getRuntime().exec("tail -f /var/log/syslog");
+			process = Runtime.getRuntime().exec("ifconfig");
+			// process = Runtime.getRuntime().exec("tail -f /var/log/syslog");
 			inputStream = process.getInputStream();
 			
 			// 一定要启动新的线程，防止InputStream阻塞处理WebSocket的线程
